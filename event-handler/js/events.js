ffac2d27643cfe06354fc6dbe9fbef4d119e5c6e
Hani Syazana <hanisyazana88@gmail.com>
2021-10-11 01:38:51 +0800

    events

---
+++ event-handler/js/events.js
@@ -0,0 +1,24 @@
+function transformUppercase (_val) {
+    return _val.toUpperCase();
+}
+
+//DOOM event listener
+let elNamaPengguna = document.getElementById("namaPengguna")
+elNamaPengguna.addEventListener("keyup",function() {
+    elNamaPengguna.value = transformUppercase(elNamaPengguna.value)
+})
+
+function checkPassword() {
+    let elKataLaluan = document.getElementById("kataLaluan")
+    let pass_val = elKataLaluan.value
+
+    if(pass_val.length > 5) {
+        alert("Password check....OK!")
+    } else {
+        alert("Password too short!")
+    }
+}
+
+//traditional
+let elKL = document.getElementById("kataLaluan")
+elKL.onblur = checkPassword
