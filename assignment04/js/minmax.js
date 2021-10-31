411ae6d96a6e6e78cbd4dde3204567c8ae1f5b6b
Hani Syazana <hanisyazana88@gmail.com>
2021-10-31 23:55:56 +0800

    minmax

---
+++ assignment04/js/minmax.js
@@ -0,0 +1,9 @@
+console.clear()
+
+function minMaxAvg(min,max) {
+  return min + ' + ' + max
+}
+let firstValue = [1,2]
+document.write("smallest number :" + firstValue[0])
+document.write("greatest number : " + firstValue[1])
+document.write("average number :" + ((firstValue[0] + firstValue[1])/2))
